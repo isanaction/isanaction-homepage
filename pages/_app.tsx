d31ad5833a700911c7d32c21c2eb20eb7221967d
IsanaKoto <>
2022-11-02 20:37:05 +0900

    GTM の設置 Fixes #10

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/Layout";
 import { theme } from "../lib/theme";
+import { TagManager } from "react-gtm-module"
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    TagManager.initialise({
+      gtmId: process.env.NEXT_PUBLIC_GTM_ID
+    })
+  }, [])
   return (
     <ChakraProvider theme={theme}>
       <Layout>
