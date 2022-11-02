9ae2c6ea582b41c2e96ae3a00070bbff25b9425d
IsanaKoto <>
2022-11-02 20:54:05 +0900

    fix:compile

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/Layout";
 import { theme } from "../lib/theme";
-import { TagManager } from "react-gtm-module"
+import TagManager from "react-gtm-module"
 import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
 
   useEffect(() => {
-    TagManager.initialise({
+    TagManager.initialize({
       gtmId: process.env.NEXT_PUBLIC_GTM_ID
     })
   }, [])
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import { Box, Center, Container, HStack, Image, VStack } from '@chakra-ui/react'
+import { Box, Center, Container, Heading, HStack, Image, Stack, VStack } from '@chakra-ui/react'
 import Twitter from '../public/icons/twitter.png'
 import Github from '../public/icons/github.png'
 import Facebook from '../public/icons/facebook.png'
 import MyImage from '../public/img/myImage.webp'
+import { motion } from 'framer-motion'
 
 const IndexPage = () => (
   <Container height={"90vh"} display={"flex"} alignItems={"center"}>
